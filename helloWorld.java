bf8800cf26c783485547fb79ca9b2f4d9b5327ad
joaoFgnds <joaofaugundes@gmail.com>
2022-05-23 23:01:26 -0300

    projeto TecBlog Desenvolvido

---
+++ helloWorld.java
@@ -0,0 +1,25 @@
+package domain;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        //(==) igual
+        // != Diferente 
+        // > Maior 
+        // < Menor
+        // >= Maior ou igual
+        // <= Menor ou igual
+        
+        //Operadores lógicos
+        // && e
+        // || ou 
+        boolean resultado; // true or false
+        
+        resultado = 1==1 ;
+        
+        System.out.println( resultado );
+        System.out.println("HelloWorld");
+        
+    }
+
+}
